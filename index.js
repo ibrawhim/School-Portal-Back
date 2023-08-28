8e2050776d03b0fa411394ba5c1bd3985c1eae18
Adeniyi Ibrahim Abiodun <ibrahimabiodun069@gmail.com>
2023-08-28 16:56:27 +0100

    portal back

--- index.js
+++ index.js
@@ -4,8 +4,16 @@ const bodyParser = require('body-parser')
 require('dotenv').config()
 let studentRouter = require('./routes/student.route')
 app.use(bodyParser.urlencoded({extended:true}))
+
+
+
+const corsOptions = {
+    origin: 'https://main-school-portal.vercel.app/',
+    optionsSuccessStatus: 200,
+  };
+
 const cors = require('cors')
-app.use(cors())
+app.use(cors(corsOptions))
 app.use(express.json())
 app.use('/student',studentRouter)
 
