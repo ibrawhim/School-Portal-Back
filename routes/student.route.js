4ddd057ad3de210b62b5a5f7080cc1e47e032025
Adeniyi Ibrahim Abiodun <ibrahimabiodun069@gmail.com>
2023-09-11 11:59:17 +0100

    portal back

--- routes/student.route.js
+++ routes/student.route.js
@@ -3,7 +3,6 @@ const router = express.Router()
 const {signUp,signIn,portal,upload} = require("../Controllers/student.controller")
 
 router.post('/signup',signUp)
-// router.get('/signin',signIn)
 router.post('/signin',signIn)
 router.get('/portal',portal)
 router.post('/image',upload)
